7a38a61cf467e33de24319ace98e6bb78d19eb9f
huseyintopuz <huseyin.topuz35@gmail.com>
2022-10-31 21:52:38 +0300

    google-clone

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import userLogo from '../../images/h.png';
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div>
+                <a href="">Gmail</a>
+            </div>
+            <div>
+                <a href="">Görseller</a>
+            </div>
+            <div className='menu'>
+                <svg viewbox="0 0 24 24">
+                    <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                </svg>
+            </div>
+            <div>
+                <img className='user-logo' src={userLogo} alt="user logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
